3d1f23f5725b206a66c1bedc217319e72635ca5c
jmisilo <jmisilo@interia.pl>
2021-10-28 00:03:25 +0200

    Imported 'vector' liblary. Defined 'solve' function.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void solve(vector<int> vect) {
+
+    int curSeqStart = 0;
+    int curSeqLen = 0;
+
+    int longestLen = 0;
+    vector<int> longestStart{};
+
+}
 
 int main()
 {

bfc63b4837cb7a12d2031ce01a5a5f9e1cef1970
jmisilo <jmisilo@interia.pl>
2021-10-28 00:20:55 +0200

    Implemented loop to iterate over array. Prepared conditions for next part of project

--- main.cpp
+++ main.cpp
@@ -11,6 +11,24 @@ void solve(vector<int> vect) {
     int longestLen = 0;
     vector<int> longestStart{};
 
+    for (int i = 0; i < vect.size() ; i++) {
+        if (vect[i] > vect[i + 1]) {
+
+            if (curSeqStart == 0) curSeqStart = i;
+            curSeqLen++;
+        } else {
+            // check if previous seq is the longest
+            // 1. it is
+
+            // 2. it is equal
+
+            // 3. else - skip
+
+            // restart variables
+            curSeqStart = 0;
+            curSeqLen = 0;
+        }
+    }
 }
 
 int main()
